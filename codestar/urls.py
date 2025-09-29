41a559ac407c1292a11c5b9cfa29f3dda2b0e801
rory-codes <rory.thompson@outlook.com>
2025-09-29 17:08:27 +0100

    Add progress database to project

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import my_blog
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='my_blog'),
+    path('blog/', blog_views.my_blog, name="blog"), # the app urls are loaded as the main urls
 ]
